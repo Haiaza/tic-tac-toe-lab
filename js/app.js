6a1e20bd2e0d7a64903a46b76b528620a39c3407
haiaza <ajames1342@gmail.com>
2024-05-30 22:19:18 -0400

    Startpoint

---
+++ js/app.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+
+
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+let board = ['','','','','','','','',''];
+let turn = 'x';
+let winner = false;
+let tie = false;
+
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+messageEl = document.querySelector('#message')
+// console.log(messageEl.textContent)
+
+const squareEls = document.querySelectorAll('.sqr') 
+// console.log(squareEls)
+
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+//Upon loading, the game state should be initialized, and a function should be called to render this game state.
+const init = () => {
+
+
+
+  render()
+}
+
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+
+
+
+
+
+
+
+/*----------------------------- Code -----------------------------*/
+
+init()
