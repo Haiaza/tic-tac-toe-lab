902731ba3971a3449fd9b3da85ca8fd2ba88cbee
haiaza <ajames1342@gmail.com>
2024-06-01 15:31:32 -0400

    updateBoard

--- js/app.js
+++ js/app.js
@@ -31,24 +31,49 @@ let tie = false;
 messageEl = document.querySelector('#message')
 // console.log(messageEl.textContent)
 
-const squareEls = document.querySelectorAll('.sqr') 
-// console.log(squareEls)
+const squareEls = document.querySelectorAll('.sqr')
+
+
+
+
 
 
 
 
 /*-------------------------------- Functions --------------------------------*/
 //Upon loading, the game state should be initialized, and a function should be called to render this game state.
+
+const render = () => {
+
+//set aside for now
+
+
+}
+
+
 const init = () => {
 
-
+// set aside for now
 
   render()
 }
 
 
+const updateBoard = () => {
+
+  // loop over board, for each element: 
+  //Using the current index of the iteration to access the corresponding square in 'squareEls'
+  board.forEach((square, idx) => {
+    if (square) {
+      squareEls[idx].style.backgroundColor = 'blue';
+
+    }
+  });
+}
 
 
+
+updateBoard()
 
 /*----------------------------- Event Listeners -----------------------------*/
 
