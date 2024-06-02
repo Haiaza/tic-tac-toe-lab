d6a0ae3738323fe1946cc08d6fae7d47b3eac9c7
haiaza <ajames1342@gmail.com>
2024-06-02 19:25:47 -0400

    Update. Im currently working on checkForTie

--- js/app.js
+++ js/app.js
@@ -11,16 +11,20 @@
 
 /*-------------------------------- Constants --------------------------------*/
 
-let board =  ['X','O','O',
+let board =  ['','','',
               '','','',
               '','',''];
-let turn = 'x';
+let turn = 'X';
 let winner = false;
 let tie = false;
 
 let player_O = 'o'
 
-
+const winningCombos = [
+  [0,1,2],[0,3,6],[0,4,8],
+  [1,4,7],[2,5,8],[3,4,5],
+  [6,7,8],[2,4,6]
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -40,13 +44,26 @@ const squareEls = document.querySelectorAll('.sqr')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+squareEls.forEach((square) => {
+  square.addEventListener('click', clickHandle)
+})
+
+
+
+
 // create a variable for the parent that has trickle down properties
 const ticTacToe = document.querySelector('section')
 
 
 ticTacToe.addEventListener('click', (event) =>{
+  //referring to the board, there is a eventHandler listening for clicks
+  //once it registers a click, the below 'event' occurs
   if(event.target.classList.contains('sqr')){
+    //if what was clicked has this class....
+    //execute the below
     console.log(`We clicked ${event.target.id}`)
+    
+
     // if(event.target.textContent === 'x'){
     //     console.log('Test complete')  Ive tested this and it isnt working thus far
     // }
@@ -72,9 +89,9 @@ const updateMessage = () =>{
     console.log(`The game is still in progress`)
   } if (winner == false && tie == true) {
     console.log(`Game over: Tie!`)
-  } /*else {
-    console.log(`The game is over! The winner is ${}`)
-  }*/
+  } else {
+    // the winner statement
+  }
 }
 
 const updateBoard = () => {
@@ -82,7 +99,12 @@ const updateBoard = () => {
   // loop over board, for each element: 
   //Using the current index of the iteration to access the corresponding square in 'squareEls'
   board.forEach((element, idx) => {
-    squareEls[idx].textContent = element
+    console.log(idx,element)
+    if(element === 'X'){
+      squareEls[idx].style.backgroundColor = 'green'
+    } else if (element === 'O'){
+      squareEls[idx].style.backgroundColor = 'cyan'
+    }
   });
 }
 
@@ -93,12 +115,76 @@ const init = () => {
   render()
 }
 
+function clickHandle(event){
+  console.log('clickd')
+  const squareIndex = event.target.id
+  placePiece(squareIndex)
+}
+
+
+function placePiece(idx){
+  board[idx] = turn
+}
+
+function checkForWinner(){
+  if(board[0] === board[1] &&
+    board[0] === board[2] &&
+    board[0] !== ''
+  ) {
+    winner = true
+  }else if(board[3] === board[4] &&
+    board[3] === board[5] &&
+    board[3] !== ''
+  ) {
+    winner = true
+  }else if(board[6] === board[7] &&
+    board[6] === board[8] &&
+    board[6] !== ''
+  ) {
+    winner = true
+  }else if(board[0] === board[3] &&
+    board[0] === board[6] &&
+    board[0] !== ''
+  ) {
+    winner = true
+  }else if(board[0] === board[4] &&
+    board[0] === board[8] &&
+    board[0] !== ''
+  ) {
+    winner = true
+  }else if(board[1] === board[4] &&
+    board[1] === board[7] &&
+    board[1] !== ''
+  ) {
+    winner = true
+  }else if(board[2] === board[5] &&
+    board[2] === board[8] &&
+    board[2] !== ''
+  ) {
+    winner = true
+  }else if(board[2] === board[4] &&
+    board[2] === board[6] &&
+    board[2] !== ''
+  ) {
+    winner = true
+  }
+}
 
 
 
+
+function checkForTie(){
+  if(winner === true){
+    return
+  }else if(board.contains = ''){
+    tie === false
+  } else {
+    tie === true
+  }
+}
 
 updateMessage()
-// updateBoard()
+updateBoard()
 // console.log(squareEls[3]);
 /*----------------------------- Code -----------------------------*/
 
